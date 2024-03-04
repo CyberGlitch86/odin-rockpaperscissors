a1c05e1eec50914da3b71ea9f941e5868894fdc5
CyberKyber <100269958+CyberGlitch86@users.noreply.github.com>
2024-03-04 21:37:16 +1100

    Add win decision function

--- js/script.js
+++ js/script.js
@@ -2,11 +2,43 @@ function getComputerChoice() {
     let rand = Math.floor(Math.random() * 3) + 1;
     switch (rand) {
         case 1:
-            return "Rock";
+            return "rock";
         case 2:
-            return "Paper";
+            return "paper";
         case 3:
-            return "Scissors";
+            return "scissors";
     }
     return undefined;
+}
+
+function playRound(playerChoice, computerChoice) {
+    playerChoice = playerChoice.toLowerCase();
+    if (playerChoice === computerChoice) {
+        return `Draw! ${playerChoice} and ${computerChoice}`
+    }
+    switch (playerChoice) {
+        case "rock":
+            switch (computerChoice) {
+                case "paper":
+                    return "You lose! Paper beats Rock";
+                case "scissors":
+                    return "You win! Rock beats Scissors";
+            }
+        case "paper":
+            switch (computerChoice) {
+                case "rock":
+                    return "You win! Paper beats Rock";
+                case "scissors":
+                    return "You lose! Scissors beats Rock";
+            }
+        case "scissors":
+            switch (computerChoice) {
+                case "rock":
+                    return "You lose! Rock beats Scissors";
+                case "paper":
+                    return "You win! Scissors beats Paper";
+            }
+        default:
+            return undefined;
+    }
 }
